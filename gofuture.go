999d318a29dc1eb67fc8dada4632e328af74499a
berkgokden <berkgokden@gmail.com>
2019-11-10 13:36:33 +0100

    Interface changed to pass timeout on Get instead of function creation

--- gofuture.go
+++ gofuture.go
@@ -1,40 +1,50 @@
 package gofuture
 
 import (
-	"fmt"
 	"reflect"
 	"time"
 )
 
+// Future type holds Result and state
 type Future struct {
 	Success          bool
 	Done             bool
 	Result           interface{}
 	InterfaceChannel <-chan interface{}
-	TimeoutChannel   <-chan time.Time
 }
 
+// Get return the result when available. This is a blocking call
 func (f *Future) Get() interface{} {
 	if f.Done {
 		return f.Result
 	}
+	f.Result = <-f.InterfaceChannel
+	f.Success = true
+	f.Done = true
+	return f.Result
+}
+
+// GetWithTimeout return the result until timeout.
+func (f *Future) GetWithTimeout(timeout time.Duration) interface{} {
+	if f.Done {
+		return f.Result
+	}
+	timeoutChannel := time.After(timeout)
 	select {
 	case res := <-f.InterfaceChannel:
-		fmt.Println(res)
 		f.Result = res
 		f.Success = true
 		f.Done = true
-		return res
-	case <-f.TimeoutChannel:
+	case <-timeoutChannel:
 		f.Result = nil
 		f.Done = true
 		f.Success = false
-		return nil
 	}
+	return f.Result
 }
 
 // FutureFunc creates a function that returns its response in future
-func FutureFunc(timeout time.Duration, implem interface{}, args ...interface{}) *Future {
+func FutureFunc(implem interface{}, args ...interface{}) *Future {
 	valIn := make([]reflect.Value, len(args), len(args))
 
 	fnVal := reflect.ValueOf(implem)
@@ -43,7 +53,6 @@ func FutureFunc(timeout time.Duration, implem interface{}, args ...interface{}) 
 		valIn[idx] = reflect.ValueOf(elt)
 	}
 	interfaceChannel := make(chan interface{}, 1)
-	timeoutChannel := time.After(timeout)
 
 	go func() {
 		res := fnVal.Call(valIn)
@@ -56,6 +65,5 @@ func FutureFunc(timeout time.Duration, implem interface{}, args ...interface{}) 
 		Done:             false,
 		Result:           nil,
 		InterfaceChannel: interfaceChannel,
-		TimeoutChannel:   timeoutChannel,
 	}
 }
--- gofuture_test.go
+++ gofuture_test.go
@@ -13,7 +13,7 @@ func TestFutureFunc(t *testing.T) {
 	x := 10
 	var elapsed time.Duration
 	start := time.Now()
-	response := gofuture.FutureFunc(10*time.Second, func() int {
+	response := gofuture.FutureFunc(func() int {
 		printTime(t)
 		time.Sleep(5 * time.Second)
 		fmt.Printf("x = %v\n", x)
@@ -38,7 +38,7 @@ func TestFutureFuncTimeOut(t *testing.T) {
 	x := 10
 	var elapsed time.Duration
 	start := time.Now()
-	response := gofuture.FutureFunc(3*time.Second, func() int {
+	response := gofuture.FutureFunc(func() int {
 		printTime(t)
 		time.Sleep(5 * time.Second)
 		fmt.Printf("x = %v\n", x)
@@ -49,7 +49,7 @@ func TestFutureFuncTimeOut(t *testing.T) {
 	t.Logf("it took %s", elapsed)
 	assert.Less(t, elapsed.Milliseconds(), (1 * time.Second).Milliseconds())
 
-	result := response.Get()
+	result := response.GetWithTimeout(3 * time.Second)
 	elapsed = time.Since(start)
 	assert.Equal(t, nil, result)
 	assert.Less(t, elapsed.Milliseconds(), (4 * time.Second).Milliseconds())
